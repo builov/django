3823759b1dfd1ae7872e4421b6c07b08bfbcc830
Dev 5 <builov@inbox.ru>
2018-07-31 22:39:59 +0600

    lesson 5

--- newsapp/server/products/forms.py
+++ newsapp/server/products/forms.py
@@ -1,9 +1,17 @@
 from django import forms
+from . import models
 
-class ProductForm(forms.Form):
+class ProductForm(forms.ModelForm):
 
-    name = forms.CharField(label='name', required=True, widget=forms.widgets.TextInput(attrs={'class': 'form-control'}))
-    image = forms.ImageField(label='image', required=False, widget=forms.widgets.ClearableFileInput(attrs={'class': 'form-control'}))
-    content = forms.CharField(label='name', required=True, widget=forms.widgets.Textarea(attrs={'class': 'form-control'}))
-    category = forms.ChoiceField(label='name', required=True, widget=forms.widgets.Select(attrs={'class': 'form-control'}))
-    cost = forms.DecimalField(label='name', required=True, widget=forms.widgets.NumberInput(attrs={'class': 'form-control'}))
+    class Meta:
+
+        model = models.Product
+        fields = ['name', 'category', 'image', 'content', 'cost']
+        widgets = {
+            'name': forms.widgets.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.widgets.Select(attrs={'class': 'form-control'}),
+            'image': forms.widgets.ClearableFileInput(attrs={'class': 'form-control-file'}),
+            'content': forms.widgets.Textarea(attrs={'class': 'form-control'}),
+            'cost': forms.widgets.NumberInput(attrs={'class': 'form-control'})
+        }
+
--- newsapp/server/products/views.py
+++ newsapp/server/products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
+
+from django.urls import reverse_lazy
 
 from django.http import HttpResponse
 
@@ -7,30 +9,35 @@ from . import models
 from . import forms
 
 
-
 def index(request):
+    # query = models.Product.objects.all()
+    query = get_list_or_404(models.Product)
 
-	user = request.user
-	
-	#query = models.Product.objects.all()
-	query = get_list_or_404(models.Product)
-	
-	return render(request, 'products/page.html', {'query': query})
-	
-	
+    return render(request, 'products/page.html', {'query': query})
+
+
 def product_detail(request, pk):
+    instance = get_object_or_404(models.Product, id=pk)
 
-	instance = get_object_or_404(models.Product, id=pk)
-	
-	return render(request, 'products/card.html', {'instance': instance})
+    return render(request, 'products/card.html', {'instance': instance})
+
 
 def product_edit(request):
 
-    print('*'*50)
+    success_url = reverse_lazy('products:index')
 
-    print(request.POST)
+    form = forms.ProductForm(request.POST, request.FILES)
 
-    form = forms.ProductForm()
+    if request.method == 'POST':
+
+        if form.is_valid():
+
+            form.save()
+
+            #return redirect(success_url)
+
+        print('*' * 50)
+
+        print(form.errors)
 
     return render(request, 'products/edit.html', {'form': form})
-
